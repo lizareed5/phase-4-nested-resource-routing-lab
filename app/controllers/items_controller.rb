6773b3fc0b0c18aa04aa17f566f74b277dbac92c
Liza McLain <lizamclain@gmail.com>
2023-02-22 23:13:11 -0600

    commit tree 21707c3bf577a26cd43533e861a7d88edaa249fe

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user, status: :ok
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user, status: :ok
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, include: :user, status: :created
+  end
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def render_not_found_response
+    render json: {error: "Item not found"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
 end
